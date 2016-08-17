135c223a51b16c31c5e9f12737c645ff56c12588
Ryan Lynch <ryan.lynch@ligo.org>
2016-08-17 00:19:41 -0400

    Outline for collection scheduler

--- collection_and_training_scheduler_eagle.py
+++ collection_and_training_scheduler_eagle.py
@@ -2,3 +2,28 @@
 
 import sys
 sys.path.insert(1,'/home/ryan.lynch/numpy/numpy-1.8.2-INSTALL/lib64/python2.7/site-packages')
+import commands
+
+#Get full gps time
+gps_time_full = int(commands.getstatusoutput('%s/lalapps_tconvert now'%run_dic['config']['LIB bin dir'])[1])
+
+#Convert full gps time into the current gps day
+gps_day_now = int(float(gps_time_full)/100000.)
+
+#Collect results from the current gps day
+???
+
+#Get previous gps day
+gps_day_last = float(np.genfromtxt(???))
+
+#Get threshold for background collection and retraining
+gps_threshold = gps_day_last * 100000. + 50000.
+
+#If past the threshold, collect background and retrain
+if gps_time_full > gps_threshold:
+	#Run background collection
+	???
+	#Run retraining
+	???
+	#Save current gps day as completed
+	???
--- update_background_eagle.py
+++ update_background_eagle.py
@@ -20,7 +20,7 @@ if __name__=='__main__':
 	parser.add_option("", "--old-back-lt", default=None, type="string", help="Path to file with old background livetime")
 	parser.add_option("", "--outdir", default=None, type="string", help="Path to directory in which to save outputs")
 	parser.add_option("", "--max-back-size", default=None, type="int", help="Maximum number of points to have in background dictionary")
-	parser.add_option("", "--new-gps-day", default=None, type="string", help="GPS day that the new day covers")
+	parser.add_option("", "--new-gps-day", default=None, type="int", help="GPS day that the new day covers")
 
 	#-----------------------------------------------------------------------
 
@@ -61,43 +61,44 @@ if __name__=='__main__':
 			#Event should be added to background
 			updated_back_dic[event] = new_back_dic[key]
 			updated_back_dic[event]['GPS Day']={}
-			updated_back_dic[event]['GPS Day']['Day'] = new_gps_day
+			updated_back_dic[event]['GPS Day']['Day'] = int(new_gps_day)
 			updated_back_dic[event]['GPS Day']['Livetime'] = new_back_lt
 			
 			#Update the number of events currently in background dictionary
+			event += 1
 			if event > max_back_size:
 				raise ValueError, "Trying to add new background events when the entirety of their GPS day does not fit in dic"
-			event += 1
 	
 	#Then add the old background events, only adding if gps day can be fully contained within the dic
-	old_gps_day = np.inf
+	old_gps_day = int(new_gps_day)
 	for key in old_back_dic:
 		#Check if we're at a new gps day
-		if old_back_dic[key]['GPS Day']['Day'] < old_gps_day:
+		if int(old_back_dic[key]['GPS Day']['Day']) < old_gps_day:
 			#At new GPS day, check how many events are in this day
-			old_gps_day = old_back_dic[key]['GPS Day']['Day']
+			old_gps_day = int(old_back_dic[key]['GPS Day']['Day'])
 			tmp_key = key
 			tmp_num = 0
-			while old_back_dic[tmp_key]['GPS Day']['Day'] == old_gps_day:
+			while int(old_back_dic[tmp_key]['GPS Day']['Day']) == old_gps_day:
 				tmp_key += 1
 				tmp_num += 1
 			#If too many events, then break loop
-			if (event - 1 + tmp_num) > max_back_size:
+			if (event + tmp_num) > max_back_size:
 				break
 			#Else, add livetime for this new gps day
-			updated_lt += old_back_dic[key]['GPS Day']['Livetime']
+			else:
+				updated_lt += float(old_back_dic[key]['GPS Day']['Livetime'])
 			
 		#Make sure we are going backwords in time
-		elif old_back_dic[key]['GPS Day']['Day'] > old_gps_day:
+		elif int(old_back_dic[key]['GPS Day']['Day']) > old_gps_day:
 			raise ValueError, "The old background dictionary isn't sorted from newest events to oldest"
 		
 		#If we've made is this far, then add the events
 		updated_back_dic[event] = old_back_dic[key]
 		
 		#Update the number of events currently in background dictionary
+		event += 1
 		if event > max_back_size:
 			raise ValueError, "Trying to add new background events when the entirety of their GPS day does not fit in dic"
-		event += 1
 	
 	#Save updated dictionary
 	pickle.dump(updated_back_dic,open('%s/collected_background_dictionary_new.pkl'%outdir,'wt'))
