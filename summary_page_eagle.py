ddb91c8a9a2e3f83bccb2e9dc9a86a2b02740b17
Ryan Lynch <ryan.lynch@ligo.org>
2016-09-15 18:28:10 -0400

    Summary page script appears to be working

--- summary_page_eagle.py
+++ summary_page_eagle.py
@@ -19,7 +19,7 @@ def cumulative_SNR_plot(snr_array_0lag,snr_array_back,livetime_0lag,livetime_bac
 	snr_sorted_back = np.sort(snr_array_back)[::-1]
 	
 	rate_above_0lag = np.arange(1,len(snr_sorted_0lag)+1) / float(livetime_0lag)
-	rate_above_back = np.arange(1,len(snr_sorted_back)+1) / float(livetime_0lag)
+	rate_above_back = np.arange(1,len(snr_sorted_back)+1) / float(livetime_back)
 
 	myfig = plt.figure()
 
@@ -55,7 +55,6 @@ def cumulative_logBSN_plot(logBSN_array_0lag,logBSN_array_back,livetime_0lag,liv
 	plt.xlabel("log10BSN")
 	plt.ylabel("Rate exceeding log10BSN value during livetime [Hz]")
 	plt.grid(True,which="both")
-	plt.xscale('log')
 	plt.yscale('log')
 	plt.title('Cumulative log10BSN rates')
 	leg = plt.legend(loc='upper right', fancybox=True)
@@ -157,12 +156,12 @@ def trig_rate_vs_time_plot(time_array,snr_array,t_start,t_stop,ifos,label,outdir
 	above_8 = time_array[snr_array >= 8.]
 	above_15 = time_array[snr_array >= 15.]
 
-	plt.plot((bins[1:]-t_stop)/86400., np.histogram(above_5,bins=bins)[0]/60., 'bo', label='SNR >= 5', markersize=3)
-	plt.plot((bins[1:]-t_stop)/86400., np.histogram(above_8,bins=bins)[0]/60., 'ro', label='SNR >= 8', markersize=3)
-	plt.plot((bins[1:]-t_stop)/86400., np.histogram(above_15,bins=bins)[0]/60., 'go', label='SNR >= 15', markersize=3)
+	plt.plot((bins[1:]-t_stop)/86400., np.histogram(above_5,bins=bins)[0]/60., 'bo', label='SNR >= 5', markersize=10)
+	plt.plot((bins[1:]-t_stop)/86400., np.histogram(above_8,bins=bins)[0]/60., 'ro', label='SNR >= 8', markersize=10)
+	plt.plot((bins[1:]-t_stop)/86400., np.histogram(above_15,bins=bins)[0]/60., 'go', label='SNR >= 15', markersize=10)
 
-	t_stop_p60_utc = commands.getstatusoutput('lalapps_tconvert -d %s'%int(t_stop+60))[1]
-	plt.xlabel("Time [days] since %s (UTC: %s), 60s bins"%(int(t_stop+60),t_stop_p60_utc))
+	t_stop_p60_date = commands.getstatusoutput('lalapps_tconvert -d %s'%int(t_stop+60))[1]
+	plt.xlabel("Time [days] since %s (Date: %s), 60s bins"%(int(t_stop+60),t_stop_p60_date))
 	plt.ylabel("Rate [Hz]")
 	plt.grid(True,which="both")
 	leg = plt.legend(loc='upper right', fancybox=True)
@@ -185,6 +184,9 @@ if __name__=='__main__':
 	parser.add_option("","--outdir", default=None, type='string', help="Path to directory in which to output plots")
 	parser.add_option("","--ifo-group", default=None, type="string", help="IFO group for which to make summary page (e.g., 'H1L1'")
 	parser.add_option("","--label", default=None, type='string', help="Label to describe what search data is being collected for")
+	parser.add_option("","--user-name", default=None, type='string', help="User name that the pipeline job is running under")
+	parser.add_option("","--run-script", default=None, type='string', help="Pipeline job name to search for to check if it is running")
+	parser.add_option("","--infodir", default=None, type='string', help="Path to directory where the index.html templates live")
 
 	#---------------------------------------------
 
@@ -195,6 +197,9 @@ if __name__=='__main__':
 	outdir = opts.outdir
 	ifo_group = opts.ifo_group
 	label = opts.label
+	user_name = opts.user_name
+	run_script = opts.run_script
+	infodir = opts.infodir
 
 	#############################################
 	
@@ -231,16 +236,19 @@ if __name__=='__main__':
 	
 	#Loop over the gps days in the week, collecting the results and livetime for the 0lag and background
 	for day in gps_days:
-		#Load in results dictionaries
-		dic_0lag = pickle.load(open(???))
-		dic_back = pickle.load(open(???))
-		
-		#Load in livetimes
-		livetime_0lag += float(np.genfromtxt(???))
-		livetime_back += float(np.genfromtxt(???))
+		try:
+			#Load in results dictionaries
+			dic_0lag = pickle.load(open('%s/%s/%s/%s_%s_%s_results.pkl'%(collectdir,'0lag',ifo_group,day,'0lag',ifo_group)))
+			dic_back = pickle.load(open('%s/%s/%s/%s_%s_%s_results.pkl'%(collectdir,'back',ifo_group,day,'back',ifo_group)))
+			
+			#Load in livetimes
+			livetime_0lag += float(np.genfromtxt('%s/%s/%s/%s_%s_%s_livetime.txt'%(collectdir,'0lag',ifo_group,day,'0lag',ifo_group)))
+			livetime_back += float(np.genfromtxt('%s/%s/%s/%s_%s_%s_livetime.txt'%(collectdir,'back',ifo_group,day,'back',ifo_group)))
+		except IOError:
+			continue
 		
 		#Loop over all events in 0lag dictionary
-		for key in dic_0lag:
+		for key in dic_0lag:			
 			SNR_lists_0lag[ifo_group] += [dic_0lag[key]['Omicron SNR']['Network']]
 			for ifo in ifos:
 				SNR_lists_0lag[ifo] += [dic_0lag[key]['Omicron SNR'][ifo]]
@@ -249,7 +257,10 @@ if __name__=='__main__':
 			freq_list_0lag += [dic_0lag[key]['frequency']['posterior mean']]
 			Q_list_0lag += [dic_0lag[key]['quality']['posterior mean']]
 			time_list_0lag += [dic_0lag[key]['gpstime']]
-			FAR_list_0lag += [dic_0lag[key]['FAR']]
+			try:
+				FAR_list_0lag += [dic_0lag[key]['FAR']]
+			except KeyError:
+				FAR_list_0lag += [np.inf]
 			
 		#Loop over all events in background dictionary
 		for key in dic_back:
@@ -265,49 +276,69 @@ if __name__=='__main__':
 	try:
 		cumulative_SNR_plot(snr_array_0lag=np.array(SNR_lists_0lag[ifo_group]),snr_array_back=np.array(SNR_lists_back[ifo_group]),livetime_0lag=livetime_0lag,livetime_back=livetime_back,ifos=ifo_group,label=label,outdir=outdir)
 	except (NameError,ValueError,IndexError):
-		???
+		os.system("> " + outdir + '/cumrate_vs_SNR_%s_%s'%(ifo_group,label))
 	
 	try:
-		trig_rate_vs_time_plot(time_array=np.array(time_list_0lag),snr_array=np.array(SNR_lists_0lag[ifo_group]),t_start=gps_days[0]*100000.,t_stop=gps_days[-1]*100000.,ifos=ifo_group,label=label,outdir=outdir)
+		trig_rate_vs_time_plot(time_array=np.array(time_list_0lag),snr_array=np.array(SNR_lists_0lag[ifo_group]),t_start=gps_days[0]*100000.,t_stop=(gps_days[-1]+1)*100000.,ifos=ifo_group,label=label,outdir=outdir)
 	except (NameError,ValueError,IndexError):
-		???
+		os.system("> " + outdir + '/glitchrate_%s_%s'%(ifo_group,label))
 	
 	for ifo in ifos:
 		try:
 			cumulative_SNR_plot(snr_array_0lag=np.array(SNR_lists_0lag[ifo]),snr_array_back=np.array(SNR_lists_back[ifo]),livetime_0lag=livetime_0lag,livetime_back=livetime_back,ifos=ifo,label=label,outdir=outdir)
 		except (NameError,ValueError,IndexError):
-			???
+			os.system("> " + outdir + '/cumrate_vs_SNR_%s_%s'%(ifo,label))
 		
 		try:
-			trig_rate_vs_time_plot(time_array=np.array(time_list_0lag),snr_array=np.array(SNR_lists_0lag[ifo]),t_start=gps_days[0]*100000.,t_stop=gps_days[-1]*100000.,ifos=ifo,label=label,outdir=outdir)
+			trig_rate_vs_time_plot(time_array=np.array(time_list_0lag),snr_array=np.array(SNR_lists_0lag[ifo]),t_start=gps_days[0]*100000.,t_stop=(gps_days[-1]+1)*100000.,ifos=ifo,label=label,outdir=outdir)
 		except (NameError,ValueError,IndexError):
-			???
+			os.system("> " + outdir + '/glitchrate_%s_%s'%(ifo,label))
 	
 	try:
 		cumulative_logBSN_plot(logBSN_array_0lag=np.array(logBSN_list_0lag),logBSN_array_back=np.array(logBSN_list_back),livetime_0lag=livetime_0lag,livetime_back=livetime_back,label=label,outdir=outdir)
 	except (NameError,ValueError,IndexError):
-		???
+		os.system("> " + outdir + '/cumrate_vs_logBSN_%s'%(label))
 
 	try:
 		cumulative_BCI_plot(BCI_array_0lag=np.array(BCI_list_0lag),BCI_array_back=np.array(BCI_list_back),livetime_0lag=livetime_0lag,livetime_back=livetime_back,label=label,outdir=outdir)
 	except (NameError,ValueError,IndexError):
-		???
+		os.system("> " + outdir + '/cumrate_vs_BCI_%s'%(label))
 
 	try:
 		cumulative_frequency_plot(freq_array_0lag=np.array(freq_list_0lag),freq_array_back=np.array(freq_list_back),livetime_0lag=livetime_0lag,livetime_back=livetime_back,label=label,outdir=outdir)
 	except (NameError,ValueError,IndexError):
-		???
+		os.system("> " + outdir + '/cumrate_vs_freq_%s'%(label))
 
 	try:
 		cumulative_Q_plot(Q_array_0lag=np.array(Q_list_0lag),Q_array_back=np.array(Q_list_back),livetime_0lag=livetime_0lag,livetime_back=livetime_back,label=label,outdir=outdir)
 	except (NameError,ValueError,IndexError):
-		???
+		os.system("> " + outdir + '/cumrate_vs_Q_%s'%(label))
 
+	#Make html containing links to the plots
+	plots_html_file = open(outdir + '/plots_html_%s.txt'%label,'wt')
+	for ifo in ifos:
+		plots_html_file.write('<a href=%s onClick="return popup(this)">\n<img src=%s width=800>\n</a>\n<br>\n\n'%('glitchrate_%s_%s.png'%(ifo,label),'glitchrate_%s_%s.png'%(ifo,label)))
+	plots_html_file.write('<a href=%s onClick="return popup(this)">\n<img src=%s width=800>\n</a>\n<br>\n\n'%('glitchrate_%s_%s.png'%(ifo_group,label),'glitchrate_%s_%s.png'%(ifo_group,label)))
+	
+	for ifo in ifos:
+		plots_html_file.write('<a href=%s onClick="return popup(this)">\n<img src=%s width=800>\n</a>\n<br>\n\n'%('cumrate_vs_SNR_%s_%s.png'%(ifo,label),'cumrate_vs_SNR_%s_%s.png'%(ifo,label)))
+	plots_html_file.write('<a href=%s onClick="return popup(this)">\n<img src=%s width=800>\n</a>\n<br>\n\n'%('cumrate_vs_SNR_%s_%s.png'%(ifo_group,label),'cumrate_vs_SNR_%s_%s.png'%(ifo_group,label)))
+	
+	plots_html_file.write('<a href=%s onClick="return popup(this)">\n<img src=%s width=800>\n</a>\n<br>\n\n'%('cumrate_vs_logBSN_%s.png'%(label),'cumrate_vs_logBSN_%s.png'%(label)))
+	plots_html_file.write('<a href=%s onClick="return popup(this)">\n<img src=%s width=800>\n</a>\n<br>\n\n'%('cumrate_vs_BCI_%s.png'%(label),'cumrate_vs_BCI_%s.png'%(label)))
+	plots_html_file.write('<a href=%s onClick="return popup(this)">\n<img src=%s width=800>\n</a>\n<br>\n\n'%('cumrate_vs_freq_%s.png'%(label),'cumrate_vs_freq_%s.png'%(label)))
+	plots_html_file.write('<a href=%s onClick="return popup(this)">\n<img src=%s width=800>\n</a>\n<br>\n\n'%('cumrate_vs_Q_%s.png'%(label),'cumrate_vs_Q_%s.png'%(label)))
+	plots_html_file.close()
+	
 	#Check if the oLIB pipeline is currently running
+	gps_check = commands.getstatusoutput('lalapps_tconvert')[1]
 	job_status_file = open(outdir + "/job_status_%s.txt"%label,'wt')
-	job_status_file.write("Job is not currently running")
+	job_status_file.write("Job is not currently running as of:\n")
+	job_status_file.write('GPS: %s\n'%gps_check)
+	job_status_file.write('Date: %s\n'%commands.getstatusoutput('lalapps_tconvert -d %s'%gps_check)[1])
 	job_status_file.close()
-	proc_list = commands.getstatusoutput('ps -Fu %s | grep %s | grep /usr/bin/python'%(???,???))[1].split('\n')
+	
+	proc_list = commands.getstatusoutput('ps -Fu %s | grep %s | grep /usr/bin/python'%(user_name,run_script))[1].split('\n')
 	for proc in proc_list:
 		proc_split = proc.split()
 		if not any(['grep' in element for element in proc_split]):
@@ -315,29 +346,34 @@ if __name__=='__main__':
 			proc_id = proc_split[1]
 			proc_loc = commands.getstatusoutput('hostname -f')[1]
 			job_status_file = open(outdir + "/job_status_%s.txt"%label,'wt')
-			job_status_file.write("Job is currently running.  Job location is %s.  Job ID is %s"%(proc_loc,proc_id))
+			job_status_file.write("Job is currently running.  Job location is %s.  Job ID is %s. As of:\n"%(proc_loc,proc_id))
+			job_status_file.write('GPS: %s\n'%gps_check)
+			job_status_file.write('Date: %s\n'%commands.getstatusoutput('lalapps_tconvert -d %s'%gps_check)[1])
 			job_status_file.close()
 			break
 
 	#Save current livetimes
-	lt_0lag_file = open(outdir + "/livetime_0lag_%s.txt"%label,'wt')
-	lt_0lag_file.write("%s s = %s days = %s years"%(livetime_0lag,livetime_0lag/3600.,livetime_0lag/86400.))
-	lt_0lag_file.close()
-	
-	lt_back_file = open(outdir + "/livetime_back_%s.txt"%label,'wt')
-	lt_back_file.write("%s s = %s days = %s years"%(livetime_back,livetime_back/3600.,livetime_back/86400.))
-	lt_back_file.close()
+	lt_file = open(outdir + "/livetime_%s.txt"%label,'wt')
+	lt_file.write("0lag: %s s = %s days = %s years\n"%(livetime_0lag,livetime_0lag/86400.,livetime_0lag/86400./365.))
+	lt_file.write("Background: %s s = %s days = %s years\n"%(livetime_back,livetime_back/86400.,livetime_back/86400./365.))
+	lt_file.close()
 	
 	#Save 25 most significant FAR triggers
-	most_sig_0lag = np.array(sorted(zip(FAR_list_0lag,time_list_0lag,logBSN_list_0lag,BCI_list_0lag,SNR_lists_0lag[ifo_group],freq_list_0lag,Q_list_0lag), key=lambda x:x[0], reverse=True))
+	most_sig_0lag = np.array(sorted(zip(FAR_list_0lag,time_list_0lag,logBSN_list_0lag,BCI_list_0lag,SNR_lists_0lag[ifo_group],freq_list_0lag,Q_list_0lag), key=lambda x:x[0]))
 	most_sig_0lag_file = open(outdir + "/most_sig_0lag_%s.txt"%label,'wt')
-	most_sig_0lag_file.write('Rank\tFAR\tTime\tlogBSN\tBCI\tNetSNR\tFreq\tQ\t\n')
-	for i,line in enumerate(array[:25]):
+	most_sig_0lag_file.write('Rank\tFAR\tTime\t\tlogBSN\t\tBCI\tNetSNR\t\tFreq\t\tQ\t\t\n')
+	for i,line in enumerate(most_sig_0lag[:25]):
 		most_sig_0lag_file.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t\n'%(i+1,line[0],line[1],line[2],line[3],line[4],line[5],line[6]))
 	most_sig_0lag_file.close()	
+	
+	#Save ifo group and label used for search
+	label_file = open(outdir + '/search_label_%s.txt'%label,'wt')
+	label_file.write('%s %s '%(ifo_group,label))
+	label_file.close()
 	
 	#-------------------------------------------------------------------
 	
 	#Build HTML page
-	os.system("rm " + outdir + '/index.html')
-	os.system("source " + outdir + '/make-index.sh')
+	for i in np.arange(1,7):
+		os.system("cp %s/index%s.html %s/"%(infodir,i,outdir))
+	os.system("cd %s; cat index1.html search_label_%s.txt index2.html job_status_%s.txt index3.html livetime_%s.txt index4.html most_sig_0lag_%s.txt index5.html plots_html_%s.txt index6.html > index.html"%(outdir,label,label,label,label,label))
